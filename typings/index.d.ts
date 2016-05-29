7d5f69012b36d7014861b622f4602f359f37a2ae
Aaron Oneal <aaron.oneal@spicypixel.com>
2016-05-29 14:36:56 -0700

    Add chai-as-promised

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,4 +2,5 @@
 /// <reference path="globals/es6-promise/index.d.ts" />
 /// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
+/// <reference path="modules/chai-as-promised/index.d.ts" />
 /// <reference path="modules/chai/index.d.ts" />
